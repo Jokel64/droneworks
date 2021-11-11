1c13ed082db03bb5210e33b9197e82b88bf6ce3d
Jakob <gh.2019-j64>
2021-11-12 00:31:47 +0100

    whatever

--- client/src/components/DronePos.vue
+++ client/src/components/DronePos.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <p>{{ msg }}</p>
+    <form>
+      <div class="form-group">
+        <label for="x">X: </label>
+        <input type="number" class="form-control" id="x" :value="x">
+      </div>
+
+      <div class="form-group">
+        <label for="y">Y: </label>
+        <input type="number" class="form-control" id="y" :value="y">
+      </div>
+
+      <div class="form-group">
+        <label for="z">Z: </label>
+        <input type="number" class="form-control" id="z" :value="z">
+      </div>
+
+      <button @click="sendMessage" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
@@ -12,11 +30,14 @@ export default {
   data() {
     return {
       msg: '',
+      x: 0,
+      y: 0,
+      z: 0,
     };
   },
   methods: {
     getMessage() {
-      const path = 'http://localhost:5000/drone_pos';
+      const path = 'http://localhost:5000/';
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
@@ -26,9 +47,20 @@ export default {
           console.error(error);
         });
     },
+    sendMessage() {
+      axios.post('http://localhost:5000/command/', { x: this.x, y: this.y, z: this.z })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   created() {
     this.getMessage();
+    setInterval(this.getMessage,
+      500);
   },
 };
 </script>
--- client/src/components/Plot.vue
+++ client/src/components/Plot.vue
@@ -2,12 +2,6 @@
   <div id="gd"></div>
 </template>
 
-<script type="module">
-const Plotly = require('plotly.js-dist');
-
-Plotly.newPlot('gd', [{ y: [1, 2, 3] }]);
-</script>
-
 <style scoped>
 
 </style>
